cab80adf453f46c220ab012a3aa400bce8f2ba2b
bf_zhang <516845590@qq.com>
2018-11-05 15:31:58 +0800

    修改IUpdateItemView

--- android-library/src/main/java/com/dxa/android/ui/view/IUpdateItemView.java
+++ android-library/src/main/java/com/dxa/android/ui/view/IUpdateItemView.java
@@ -1,7 +1,57 @@
 package com.dxa.android.ui.view;
 
+import java.util.List;
+
 /**
  * 更新Item的View视图
  */
-public interface IUpdateItemView extends IRefreshView, ILoadMoreView {
+public interface IUpdateItemView<Item> extends IRefreshView<Item>, ILoadMoreView<Item> {
+
+    /**
+     * 刷新
+     */
+    @Override
+    void refreshStart();
+
+    /**
+     * 刷新完成
+     */
+    @Override
+    void refreshCompleted();
+
+    /**
+     * 刷新失败
+     *
+     * @param errorMsg 错误信息
+     */
+    @Override
+    void refreshError(String errorMsg);
+
+    /**
+     * 添加Item
+     *
+     * @param items
+     */
+    @Override
+    void addItems(List<Item> items);
+
+    /**
+     * 加载更多
+     */
+    @Override
+    void loadMoreStart();
+
+    /**
+     * 加载更多完成
+     */
+    @Override
+    void loadMoreCompleted();
+
+    /**
+     * 加载更多失败
+     *
+     * @param errorMsg 错误信息
+     */
+    @Override
+    void loadMoreFailed(String errorMsg);
 }
--- android-library/src/main/java/com/dxa/android/ui/view/IUpdateView.java
+++
@@ -1,8 +0,0 @@
-package com.dxa.android.ui.view;
-
-/**
- * 更新View的视图
- */
-
-public interface IUpdateView extends IRefreshView, ILoadMoreView {
-}
